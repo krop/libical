46876a3a95ed52803a2326f5de7debd84b8c3903
Christophe Giboudeaux <christophe@krop.fr>
2018-11-07 18:53:00 +0100

    Refresh FindGObjectIntrospect.cmake
    Note: Although possible, the module doesn't look for the gobjectintrospection version. libical requires gobjectintrospection >= 0.6.7.
    in modern distributions, the gobjectintrospection major version is 1, which makes FPHSA complain.

--- cmake/modules/FindGObjectIntrospection.cmake
+++ cmake/modules/FindGObjectIntrospection.cmake
@@ -1,20 +1,62 @@
-# - try to find gobject-introspection
+#.rst:
+# FindGObjectIntrospection
+# ------------------------
 #
-# Once done this will define
+# Find the GObject-introspection library and utilities
 #
-#  GObjectIntrospection_FOUND - system has gobject-introspection
-#  GObjectIntrospection_SCANNER - the gobject-introspection scanner, g-ir-scanner
-#  GObjectIntrospection_COMPILER - the gobject-introspection compiler, g-ir-compiler
-#  GObjectIntrospection_GENERATE - the gobject-introspection generate, g-ir-generate
-#  GObjectIntrospection_GIRDIR
-#  GObjectIntrospection_TYPELIBDIR
-#  GObjectIntrospection_CFLAGS
-#  GObjectIntrospection_LIBS
+# Once done this will define:
 #
+#  ``GObjectIntrospection_FOUND``
+#     System has gobject-introspection
+#
+#  ``GObjectIntrospection_SCANNER``
+#     The gobject-introspection scanner, g-ir-scanner
+#
+#  ``GObjectIntrospection_COMPILER``
+#     The gobject-introspection compiler, g-ir-compiler
+#
+#  ``GObjectIntrospection_GENERATE``
+#     The gobject-introspection generate, g-ir-generate
+#
+#  ``GObjectIntrospection_GIRDIR``
+#
+#  ``GObjectIntrospection_TYPELIBDIR``
+#
+#  ``GObjectIntrospection_CFLAGS``
+#     Additional gobject-introspection compiler flags
+#
+#  ``GObjectIntrospection_LIBRARIES``
+#     The gobject-introspection libraries
+#
+#=============================================================================
 # Copyright (C) 2010, Pino Toscano, <pino@kde.org>
 #
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
+
+find_package(PkgConfig QUIET)
 
 macro(_GIR_GET_PKGCONFIG_VAR _outvar _varname)
   execute_process(
@@ -30,25 +72,28 @@ macro(_GIR_GET_PKGCONFIG_VAR _outvar _varname)
     separate_arguments(_result)
     set(${_outvar} ${_result} CACHE INTERNAL "")
   endif()
-endmacro(_GIR_GET_PKGCONFIG_VAR)
+endmacro()
 
-find_package(PkgConfig)
-if(PKG_CONFIG_FOUND)
-  if(PACKAGE_FIND_VERSION_COUNT GREATER 0)
-    set(_gir_version_cmp ">=${PACKAGE_FIND_VERSION}")
-  endif()
-  pkg_check_modules(_pc_gir gobject-introspection-1.0${_gir_version_cmp})
-  if(_pc_gir_FOUND)
-    set(GObjectIntrospection_FOUND TRUE)
-    _gir_get_pkgconfig_var(GObjectIntrospection_SCANNER "g_ir_scanner")
-    _gir_get_pkgconfig_var(GObjectIntrospection_COMPILER "g_ir_compiler")
-    _gir_get_pkgconfig_var(GObjectIntrospection_GENERATE "g_ir_generate")
-    _gir_get_pkgconfig_var(GObjectIntrospection_GIRDIR "girdir")
-    _gir_get_pkgconfig_var(GObjectIntrospection_TYPELIBDIR "typelibdir")
-    set(GObjectIntrospection_CFLAGS "${_pc_gir_CFLAGS}")
-    set(GObjectIntrospection_LIBS "${_pc_gir_LIBS}")
-  endif()
+pkg_check_modules(PC_GIR gobject-introspection-1.0 QUIET)
+
+set(GObjectIntrospection_CFLAGS "${PC_GIR_CFLAGS}")
+set(GObjectIntrospection_LIBRARIES "${PC_GIR_LIBRARIES}")
+
+if(PC_GIR_FOUND)
+  _gir_get_pkgconfig_var(GObjectIntrospection_SCANNER "g_ir_scanner")
+  _gir_get_pkgconfig_var(GObjectIntrospection_COMPILER "g_ir_compiler")
+  _gir_get_pkgconfig_var(GObjectIntrospection_GENERATE "g_ir_generate")
+  _gir_get_pkgconfig_var(GObjectIntrospection_GIRDIR "girdir")
+  _gir_get_pkgconfig_var(GObjectIntrospection_TYPELIBDIR "typelibdir")
 endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GObjectIntrospection
+  FOUND_VAR GObjectIntrospection_FOUND
+  REQUIRED_VARS GObjectIntrospection_SCANNER GObjectIntrospection_COMPILER
+      GObjectIntrospection_GENERATE GObjectIntrospection_GIRDIR
+      GObjectIntrospection_TYPELIBDIR GObjectIntrospection_LIBRARIES
+)
 
 mark_as_advanced(
   GObjectIntrospection_SCANNER
@@ -57,5 +102,5 @@ mark_as_advanced(
   GObjectIntrospection_GIRDIR
   GObjectIntrospection_TYPELIBDIR
   GObjectIntrospection_CFLAGS
-  GObjectIntrospection_LIBS
+  GObjectIntrospection_LIBRARIES
 )
